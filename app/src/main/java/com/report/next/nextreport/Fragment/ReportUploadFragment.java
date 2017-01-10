634e9dec2477f218291ef4cf97f32c734c03a387
sake1 <timothydickys@gmail.com>
2017-01-10 18:34:01 +0700

    10/01/17 - add upload report

--- app/src/main/java/com/report/next/nextreport/Fragment/ReportUploadFragment.java
+++ app/src/main/java/com/report/next/nextreport/Fragment/ReportUploadFragment.java
@@ -6,14 +6,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.report.next.nextreport.Activity.HomeActivity;
 import com.report.next.nextreport.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Upload_form.jpg
  */
 
 public class ReportUploadFragment extends Fragment {
+
+    @BindView(R.id.fru_input_fullname) EditText fullname;
+    @BindView(R.id.fru_input_email) EditText email;
+    @BindView(R.id.fru_input_phone_number) EditText phoneNumber;
+    @BindView(R.id.fru_input_report_title) EditText title;
+    @BindView(R.id.fru_input_description) EditText description;
+
+    private HomeActivity main;
+
+    @OnClick(R.id.fru_trigger_upload)
+    public void upload() {
+        if(validate()) {
+            main.changeFragment(new HomeFragment(), true);
+        }
+    }
+
+    private boolean validate() {
+        if(fullname.getText().toString().equals("") ||
+                email.getText().toString().equals("") ||
+                phoneNumber.getText().toString().equals("") ||
+                title.getText().toString().equals("") ||
+                description.getText().toString().equals("")) {
+            Toast.makeText(main, "Please Fill All Field", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
 
     public ReportUploadFragment() {
         // Required empty public constructor
@@ -23,7 +58,11 @@ public class ReportUploadFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_upload, container, false);
+        View thisFragmentView = inflater.inflate(R.layout.fragment_report_upload, container, false);
+        main = (HomeActivity) getActivity();
+        ButterKnife.bind(this, thisFragmentView);
+
+        return thisFragmentView;
     }
 
 }
