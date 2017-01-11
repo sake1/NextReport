b5323f0b1e3af017ea765b2e449b60d1c5ece27a
TimotiusEk <eka.valentino@gmail.com>
2017-01-11 15:49:36 +0700

    show Ranking and add ReportDetailFragment

--- app/src/main/java/com/report/next/nextreport/Fragment/ReportRankingFragment.java
+++ app/src/main/java/com/report/next/nextreport/Fragment/ReportRankingFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.report.next.nextreport.R;
+import com.report.next.nextreport.Ranking;
+import com.report.next.nextreport.RankingAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Ranking.jpg
  */
 
 public class ReportRankingFragment extends Fragment {
+
+    private int rank = 0;
 
     public ReportRankingFragment() {
         // Required empty public constructor
@@ -24,6 +31,27 @@ public class ReportRankingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_report_ranking, container, false);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        populateData();
+    }
+
+    public void populateData(){
+        ArrayList<Ranking> arrayOfRankings = new ArrayList<>();
+
+
+        arrayOfRankings.add(new Ranking(++rank, "Eka", "Sales", "Jakarta", 25, 30));
+        arrayOfRankings.add(new Ranking(++rank, "Bejo", "Engineer", "New Delhi", 23, 27));
+        arrayOfRankings.add(new Ranking(++rank, "Marcus", "PR", "London", 20, 24));
+        arrayOfRankings.add(new Ranking(++rank, "Tarjo", "Sales", "Melbourne", 15, 16));
+        arrayOfRankings.add(new Ranking(++rank, "Paijo", "Doctor", "Kuala Lumpur", 5, 3));
+        arrayOfRankings.add(new Ranking(++rank, "Pok Inem", "IT", "Bandung", 2, 1));
+
+        ListView listView = (ListView) getActivity().findViewById(R.id.rank_list);
+        RankingAdapter adapter = new RankingAdapter(getActivity(), arrayOfRankings);
+        listView.setAdapter(adapter);
     }
 
 }
